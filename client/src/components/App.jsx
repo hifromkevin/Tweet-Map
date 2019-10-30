86f25b818cc30f2a4c2aff6001bd2b8a02ed3160
hifromkevin <lam.kevin86@gmail.com>
2019-10-30 16:30:08 -0700

    Connects App, Map, and Tweets components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import Tweets from './Tweets.jsx';
+import Map from './Map.jsx';
+
 export default function App() {
   return (
-    <div>
-      <h1>I am an app!</h1>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm">
+          <Tweets />
+        </div>
+        <div className="col-sm">
+          <Map />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ client/src/components/Tweets.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Tweets extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Tweets</h1>
+      </div>
+    )
+  }
+}
