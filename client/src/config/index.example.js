0903080632b1a31a0dc7bf92b8d781a32e6cbb8d
hifromkevin <lam.kevin86@gmail.com>
2019-10-30 18:36:32 -0700

    Sets up diretory to save API keys

---
+++ client/src/config/index.example.js
@@ -0,0 +1,11 @@
+// Enter your API credentials into this file, and remove '.example' from this file name
+
+module.exports = {
+  twitter: {
+    consumer_key: '[ENTER TWITTER CONSUMER KEY]',
+    consumer_secret: '[ENTER TWITTER CONSUMER SECRET]',
+    access_token_key: '[ENTER TWITTER ACCESS TOKEN KEY]',
+    access_token_secret: '[ENTER TWITTER ACCESS TOKEN SECRET]'
+  },
+  google: '[ENTER GOOGLE API KEY]'
+};
