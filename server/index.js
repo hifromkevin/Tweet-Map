664b14292031448b0767242ba94d7295fdc0be59
hifromkevin <lam.kevin86@gmail.com>
2019-10-30 18:36:14 -0700

    Sets up route to get Tweets

--- server/index.js
+++ server/index.js
@@ -4,4 +4,17 @@ const port = 3005;
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+// Get Tweets
+var Twitter = require('twitter');
+var config = require('../client/src/config');
+var client = new Twitter(config.twitter);
+
+app.get('/tweets', (req, res) => {
+  var params = {screen_name: 'hifromkevin'};
+  client.get('statuses/user_timeline', params, function(err, data, response) {
+    res.json(data);
+    if (err) console.log('ERR', err);
+  });
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}!`));
