998d09efbc5dbc5098d8667ac1af85b60519a270
hifromkevin <lam.kevin86@gmail.com>
2019-10-31 09:26:35 -0700

    Makes minor styling tweaks

--- client/src/components/Tweet.jsx
+++ client/src/components/Tweet.jsx
@@ -7,7 +7,7 @@ export default function Tweet({tweet}) {
         <div className="col-sm-2">
         <img src={tweet.user.profile_image_url} alt={tweet.user.screen_name} className="tweet__avatar" />
         </div>
-        <div className="col-sm-10">
+        <div className="col-sm-10 tweet__content">
           <p className="tweet__text"><strong>{tweet.user.screen_name} is all like: </strong> {tweet.text} </p>
         </div>
       </div>
